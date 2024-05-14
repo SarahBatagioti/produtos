8795a09b5fc5591aa2547d687f33f4a24085b7f6
Sarah Batagioti <sarah.montuanibt@gmail.com>
2024-05-14 12:10:12 -0300

    Inserção formulário e tabela no banco

---
+++ script.sql
@@ -0,0 +1,10 @@
+create database fastfood;
+use fastfood;
+
+create table produto (
+    id int auto_increment primary key,
+    nomeProduto varchar(255) not null,
+    descricaoProduto varchar(255) not null,
+    imagemProduto VARCHAR(255)not null,
+    status ENUM('peding', 'completed') default 'peding'
+);
